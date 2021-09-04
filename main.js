e2aca55eb667c1f5a94c6dadac9ca4bc16de1ea6
Avneet-Randhawa <76783244+Avneet-Randhawa@users.noreply.github.com>
2021-09-04 21:02:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-console.log("Mam When You will run the functions, it is possible that text is not vissible");
-console.log("This is because text is similar to background color");
-
 menu_list_array = ["Chicken Tandoori Pizza",
                    "Veg Supreme  Pizza",
                    "Paneer Tikka Pizza",
                    "Deluxe Veggie Pizza",
-                   "Veg Extravaganza Pizza"
+                   "Veg Extravaganza Pizza",
+                   "Margherita Pizza"
                   ];
 
 function getmenu(){
@@ -20,14 +18,13 @@ function getmenu(){
 } 
 
 function add_item(){
-    console.log("Add Item Function is working but not sure about Stylesheet");
     var htmldata;
     var item = document.getElementById("add_item").value;
     menu_list_array.push(item);
     menu_list_array.sort();
     htmldata = "<section class='cards'>";
     for(var i=0;i < menu_list_array.length;i++){
-        htmldata = htmldata + "<div class='cards'>" + "<img src='images/pizzaImg.png'" + menu_list_array[i] + "</div>";
+        htmldata = htmldata + "<div class='card'>" + "<img src='images/pizzaImg.png'>" + menu_list_array[i] + "</div>";
         console.log("Text Displayed")
     }
     htmldata = htmldata + "</section>";
